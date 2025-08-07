e914f063fde020d53889d46e8635c3968a9a46b4
mentos-MA <medoamr816@gmail.com>
2025-08-07 23:37:48 +0300

    Update main1.cpp

--- task 1/main1.cpp
+++ task 1/main1.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    cin>>n ;
+    int arr[n];
+    double sum = 0 ;
+    long double volume ;
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>arr[i] ;
+        sum+=arr[i] ;
+
+    }
+    volume=((sum/100)/n)*100 ;
+    cout<<fixed<<setprecision(4);
+    cout<< volume <<endl;
     return 0;
 }
